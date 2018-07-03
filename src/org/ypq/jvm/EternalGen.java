4af9dbbada5f0fa737703716055b917ac7442963
sabersword <godigmh@gmail.com>
2018-07-03 21:32:57 +0800

    测试java的GC,产生大量垃圾,导致年轻代和老年代空间都满了

--- src/org/ypq/jvm/EternalGen.java
+++ src/org/ypq/jvm/EternalGen.java
@@ -1,12 +1,16 @@
-import java.util.ArrayList;
+package org.ypq.jvm;
+
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
-public class Main {
-
+/**
+ * 测试java的GC,产生大量垃圾,导致年轻代和老年代空间都满了
+ */
+public class EternalGen {
     public static void main(String[] args) {
         Runtime runtime = Runtime.getRuntime();
+        System.out.println("availableProcessors=" + runtime.availableProcessors());
         System.out.println("maxMemory=" + runtime.maxMemory() / 1024.0 / 1024 + "M");
         System.out.println("totalMemory=" + runtime.totalMemory() / 1024.0 / 1024 + "M");
         Random random = new Random();
@@ -14,11 +18,12 @@ public class Main {
         List<String> stringList = new LinkedList<String>();
 
         for(long i = 0; ;i++) {
-//            System.out.println("i=" + i + ",str.length=" + str.length());
+//            System.out.println("i=" + i + ",str.length=" + str);
             stringList.add(str += String.valueOf(random.nextInt(9999999)));
+//            str = String.valueOf(random.nextInt(999999999));
 //            str.intern();
             try {
-                Thread.sleep(10);
+                Thread.sleep(1);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
