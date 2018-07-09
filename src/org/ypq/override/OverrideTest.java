9db4141abe95621f5463c55ab08316dc98f4fbe0
sabersword <godigmh@gmail.com>
2018-07-09 21:35:58 +0800

    加上重写的注释

--- src/org/ypq/override/OverrideTest.java
+++ src/org/ypq/override/OverrideTest.java
@@ -1,5 +1,10 @@
 package org.ypq.override;
 
+
+/**
+ * 重写返回值类型必须一致，那么Java重写返回值类型必须一样吗？
+ * java 5或者以前，必须一样，java 7 java 8可以不同，但是必须是父类返回值的派生类。
+ */
 public class OverrideTest {
 
     public static void main(String[] args) {
@@ -7,4 +12,5 @@ public class OverrideTest {
         ConcurrentHashMap concurrentHashMap = new ConcurrentHashMap(keySetView);
         KeySet keySet = concurrentHashMap.keySet();
     }
+
 }
