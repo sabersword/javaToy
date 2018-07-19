bacb47db7521fd3f68cd8bf1ea6741509e9cdae4
sabersword <godigmh@gmail.com>
2018-07-19 18:54:04 +0800

    包装异常的时候应该是e,不应该e.getcause()

--- src/org/ypq/exception/ExceptionTest.java
+++ src/org/ypq/exception/ExceptionTest.java
@@ -18,8 +18,8 @@ public class ExceptionTest {
             double result = 1 / 0;
             System.out.println(result);
         } catch (ArithmeticException arithmeticException) {
-            throw new DefCheckException(arithmeticException);
-//            throw new DefCheckException("产生了自定义 检查异常", arithmeticException);
+//            throw new DefCheckException(arithmeticException);
+            throw new DefCheckException("产生了自定义 检查异常", arithmeticException.getCause());
         }
     }
 
@@ -27,8 +27,8 @@ public class ExceptionTest {
         try {
             checkExceptionMethod();
         } catch (Exception e) {
-//            throw new DefRTException("产生了自定义 运行时异常", e);
-            throw new DefRTException(e);
+            throw new DefRTException("产生了自定义 运行时异常", e);
+//            throw new DefRTException(e);
         }
     }
 
