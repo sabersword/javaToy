77fa636a695a183718565b5ca4e03e994e32ebe4
sabersword <godigmh@gmail.com>
2018-08-29 17:35:54 +0800

    尝试writeandflush

--- src/main/java/org/ypq/netty/EchoServerHandler.java
+++ src/main/java/org/ypq/netty/EchoServerHandler.java
@@ -2,6 +2,8 @@ package org.ypq.netty;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
@@ -27,21 +29,23 @@ public class EchoServerHandler extends ChannelInboundHandlerAdapter {
         String currentTime = new Date(System.currentTimeMillis()).toString();
         System.out.println("服务端当前的时间:"+currentTime);
         ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
-        ctx.write(resp);
+        ctx.writeAndFlush(resp).addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture future) throws Exception {
+                System.out.println("服务端writeAndFlush完成");
+            }
+        });
     }
 
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-        // TODO Auto-generated method stub
-        System.out.println("server 数据处理完毕");
-        ctx.flush();
+        System.out.println("server 调用channelReadComplete");
+//        ctx.flush();
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        // TODO Auto-generated method stub
         cause.printStackTrace();
-
         ctx.close();
     }
 }
