087c656e8eda8da7275d4908ebc3664ffeb65deb
sabersword <godigmh@gmail.com>
2018-08-31 19:25:30 +0800

    netty实现http

--- src/main/java/org/ypq/netty/EchoServer.java
+++ src/main/java/org/ypq/netty/EchoServer.java
@@ -7,6 +7,7 @@ import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
 
 public class EchoServer {
     private final int port;
@@ -26,7 +27,7 @@ public class EchoServer {
                 .childHandler(new ChannelInitializer<Channel>() {
                     @Override
                     protected void initChannel(Channel ch) throws Exception {
-                        // TODO Auto-generated method stub
+                        ch.pipeline().addLast(new HttpServerCodec());
                         ch.pipeline().addLast(new EchoServerHandler());
                     }
                 });
--- src/main/java/org/ypq/netty/EchoServerHandler.java
+++ src/main/java/org/ypq/netty/EchoServerHandler.java
@@ -6,8 +6,17 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpResponse;
 
 import java.util.Date;
+
+import static io.netty.handler.codec.http.HttpHeaders.Names.CONTENT_LENGTH;
+import static io.netty.handler.codec.http.HttpHeaders.Names.CONTENT_TYPE;
+import static io.netty.handler.codec.http.HttpHeaders.Values.KEEP_ALIVE;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+import static org.springframework.http.HttpHeaders.CONNECTION;
 
 public class EchoServerHandler extends ChannelInboundHandlerAdapter {
 
@@ -15,21 +24,28 @@ public class EchoServerHandler extends ChannelInboundHandlerAdapter {
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         System.out.println("server 开始读取数据。。。");
 
-        ByteBuf buf = (ByteBuf)msg;
+//        ByteBuf buf = (ByteBuf)msg;
 
-        byte[] req = new byte[buf.readableBytes()];
+//        byte[] req = new byte[buf.readableBytes()];
 
-        buf.readBytes(req);
+//        buf.readBytes(req);
 
-        String body = new String(req, "UTF-8");
+//        String body = new String(req, "UTF-8");
 
-        System.out.println("接收客户端数据:" + body);
+//        System.out.println("接收客户端数据:" + body);
         //向客户端发送数据
         System.out.println("server向client发送数据。。。");
         String currentTime = new Date(System.currentTimeMillis()).toString();
         System.out.println("服务端当前的时间:"+currentTime);
         ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
-        ctx.writeAndFlush(resp).addListener(new ChannelFutureListener() {
+
+        String str = "{\"success\":true,\"message\":\"密码即将过期\",\"detail\":null,\"data\":null}";
+        FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(str.getBytes()));
+        response.headers().set(CONTENT_TYPE, "application/json;charset=UTF-8");
+        response.headers().set(CONTENT_LENGTH, response.content().readableBytes());
+        response.headers().set(CONNECTION, KEEP_ALIVE);
+
+        ctx.writeAndFlush(response).addListener(new ChannelFutureListener() {
             @Override
             public void operationComplete(ChannelFuture future) throws Exception {
                 System.out.println("服务端writeAndFlush完成");
